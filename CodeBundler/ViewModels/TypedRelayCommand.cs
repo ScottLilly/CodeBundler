f1fdc8588afa6163835ceb9c400183e648d7d8f4
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2025-04-26 11:05:19 -0400

    Added "About" window

---
+++ CodeBundler/ViewModels/TypedRelayCommand.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Input;
+
+namespace CodeBundler.ViewModels;
+
+// This is a generic implementation of ICommand that allows you to
+// specify the type of the parameter to pass into the function that is executed.
+public class TypedRelayCommand<T> : ICommand
+{
+    private readonly Action<T> _execute;
+    private readonly Predicate<T> _canExecute;
+
+    public TypedRelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object parameter)
+    {
+        return _canExecute == null || _canExecute((T)parameter);
+    }
+
+    public void Execute(object parameter)
+    {
+        _execute((T)parameter);
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    public void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
